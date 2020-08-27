33520954da9d9e1048e02a972472fa7fb2a766c9
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2020-08-27 14:30:13 -0300

    configurações inicias para o spring funcionar

---
+++ dspi/src/main/java/org/dspi/conf/AppWebConfiguration.java
@@ -0,0 +1,28 @@
+package org.dspi.conf;
+
+import org.dspi.controllers.IndexController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={IndexController.class})
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/views/");
+	    resolver.setSuffix(".jsp");
+	    return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	    configurer.enable();
+	}
+
+}
